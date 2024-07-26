94c53a3d99f243b81c09a57ec9963ba168720bd1
Pramil <pramildhungana7@gmail.com>
2024-07-26 17:57:50 +0545

    UI changes and js file added

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const currentBlc = document.getElementsByClassName("current-blc");
+const totalIncome = document.getElementsByClassName("total-income");
+const profileImg = document.getElementsByTagName("Img");
+const profileName = document.getElementsByClassName("profileName");
+const inputExpenses = document.getElementsByTagName("input");
+const addCategory = document.getElementsByClassName("Category-type");
+const addIncome = document.getElementsByClassName("plus");
+
+const firstDataCato = document.getElementsByClassName("data-feed-first-catog");
+const firstDataPrice = document.getElementsByClassName("data-feed-first-price");
+const secondDataCato = document.getElementsByClassName("data-feed-second-catog");
+const secondDataPrice = document.getElementsByClassName("data-feed-second-price");
+const thirdDataCato = document.getElementsByClassName("data-feed-third-catog");
+const thirdDataPrice = document.getElementsByClassName("data-feed-third-price");
+
+console.log('hello world');
